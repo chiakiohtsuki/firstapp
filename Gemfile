c774b0ea929e26a50d3a5677c47173cdb4c3dd2e
ootsuki chiaki <chiakinyankoko@gmail.com>
2020-03-10 01:32:56 +0900

    users add

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'sqlite3', '1.3.13'
+  gem 'sqlite3', '~> 1.4'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
 Rails.application.routes.draw do
+  resources :users
   root 'application#hello'
 end

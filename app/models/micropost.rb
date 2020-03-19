9e053b7fa5d6858906889a07dd9f2dfa734234eb
ootsuki chiaki <chiakinyankoko@gmail.com>
2020-03-19 17:03:30 +0900

    finaly toyapp

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ApplicationRecord
+    belongs_to :user
+    validates :content, length: { maximum: 140 }
+                presence: true
+  end
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
-  root 'application#hello'
+  root 'users#index'
 end
+
